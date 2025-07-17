0c748e41f3fdf9c128548058183e8c74feecc1ab
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-07-17 15:16:26 +0530

    Instruct the express app to listen on port 3000 & Initialized the express app

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, {Express} from "express";
+
+const app: Express = express();
+
+app.use(express.json());
+
+const allowedOrigins = [
+    'http://localhost:5173',
+];
+
+
+
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import dotenv from "dotenv";
+import app from "./app";
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+})
 
 
